2913b96691eae1639af521ac4b1066f5ab768d40
TheClashFruit <clash.fruit123@gmail.com>
2025-03-09 16:04:01 +0100

    feat: add more code style configs

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'none',
+  tabWidth: 2,
+  singleQuote: true,
+  endOfLine: 'auto'
+};
+
+export default config;
